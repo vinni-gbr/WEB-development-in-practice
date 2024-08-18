61783d1eaa2fae4eee4bc1665e1fc9f1f5045c40
Vinicius Gabriel <157854483+vinni-gbr@users.noreply.github.com>
2024-08-18 19:48:51 -0300

    APNP
    APNP

---
+++ tela de login/index.js
@@ -0,0 +1,12 @@
+
+        let isFlipped = false;
+
+        function flipScreen() {
+            const container = document.getElementById('flipContainer');
+            if (isFlipped) {
+                container.style.transform = 'rotateY(0deg)';
+            } else {
+                container.style.transform = 'rotateY(180deg)';
+            }
+            isFlipped = !isFlipped;
+        }
